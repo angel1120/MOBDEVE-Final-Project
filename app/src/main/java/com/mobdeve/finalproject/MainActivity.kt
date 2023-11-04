6f2e1292398183d9dea9133b52566a754f520639
angel1120 <angel_lou_yabut@dlsu.edu.ph>
2023-11-04 20:52:01 +0800

    edited start page

--- app/src/main/java/com/mobdeve/finalproject/MainActivity.kt
+++ app/src/main/java/com/mobdeve/finalproject/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.add_edit_saved_place)
+        setContentView(R.layout.activity_main)
     }
 }
