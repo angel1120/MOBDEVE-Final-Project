1d6a5df71ef6646a77373b2144dd95c3d216f95c
crunchypotato63 <shanna_tan@dlsu.edu.ph>
2023-11-24 19:34:12 +0800

    marker and maps move when area clicked, choice stored in chosen destination

--- app/src/main/java/com/mobdeve/finalproject/MapsMainActivity.kt
+++ app/src/main/java/com/mobdeve/finalproject/MapsMainActivity.kt
@@ -1,10 +1,13 @@
 package com.mobdeve.finalproject
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AutoCompleteTextView
 import android.widget.Button
+import android.widget.SearchView
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -12,12 +15,17 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 import com.mobdeve.finalproject.databinding.ActivityMapsMainBinding
 
 class MapsMainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsMainBinding
+//    private lateinit var searchView: SearchView
 
     private lateinit var slideMenuSearch: View
     private lateinit var slideMenuSearchExpandButton: Button
@@ -27,8 +35,15 @@ class MapsMainActivity : AppCompatActivity(), OnMapReadyCallback {
     private var slideMenuState: Int = 0 // 0 - down, 1 - up
     private var slideMenuDistance: Float = 0F
 
+    private lateinit var chosenDestination: LatLng
+    private lateinit var autocompleteFragment: AutocompleteSupportFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val appInfo = applicationContext.packageManager.getApplicationInfo(applicationContext.packageName, PackageManager.GET_META_DATA)
+        val bundle = appInfo.metaData
+        Places.initialize(applicationContext, bundle.getString("com.google.android.geo.API_KEY"))
 
         binding = ActivityMapsMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -37,6 +52,12 @@ class MapsMainActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        //Setup AutocompleteSupportFragment for search
+
+        autocompleteFragment = AutocompleteSupportFragment()
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG))
+        autocompleteFragment.setHint("Search for a location")
 
         binding.buttonSettings.setOnClickListener({
             var intent = Intent(applicationContext, SettingsActivity::class.java)
@@ -124,7 +145,47 @@ class MapsMainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         // Add a marker in Sydney and move the camera
         val dlsu = LatLng(14.5638, 120.9932)
-        mMap.addMarker(MarkerOptions().position(dlsu).title("DLSU"))
+        val marker = mMap.addMarker(MarkerOptions().position(dlsu))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(dlsu, 15.0f))
+
+        mMap.setOnMapClickListener { latLng ->
+            chosenDestination = latLng
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+            marker?.position = latLng
+        }
+
+        autocompleteFragment.setOnPlaceSelectedListener(object :
+            com.google.android.libraries.places.widget.listener.PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                // Handle the selected place
+                val location = place.latLng
+                marker?.position = location
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(location))
+                chosenDestination = location
+            }
+
+            override fun onError(status: com.google.android.gms.common.api.Status) {
+                // Handle the error
+            }
+        })
+
+        supportFragmentManager.beginTransaction().replace(R.id.frameLayout, autocompleteFragment)
+            .commit()
+//        searchView = findViewById(R.id.searchView)
+//        searchView.setOnQueryTextListener( object : SearchView.OnQueryTextListener {
+//            override fun onQueryTextSubmit(query: String?): Boolean {
+//                // Handle search submission if needed
+//                return false
+//            }
+//
+//            override fun onQueryTextChange(newText: String?): Boolean {
+//                if (newText.isNullOrBlank()) {
+//                    autocompleteFragment.view?.visibility = android.view.View.VISIBLE
+//                } else {
+//                    autocompleteFragment.view?.visibility = android.view.View.GONE
+//                }
+//                return true
+//            }
+//        })
     }
 }
